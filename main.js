6c7ad10b5aad188820bbdd5b38605e35af94e807
Mircea Sirghi <sirghim@ebrd.com>
2025-02-02 09:57:26 +0200

    removing comments

--- main.js
+++ main.js
@@ -9,21 +9,10 @@ var openFile = function(event) {
 	console.log('test');
 	reader.onload = function(){
 		
-		// //convert string to blob
-		// const str2blob = (txt) => new Blob([txt]);
-		// //blob converted to URL
-		// const url = URL.createObjectURL(str2blob('console.log("string worker")'));
-		
 	  dataURL = reader.result;
 	  console.log(dataURL);
 	  
-	  
-	  
-	  // var output = document.getElementById('output');
-	  // output.src = dataURL;
-	  
 	};
-	// reader.readAsText(input.files[0]);
 	reader.readAsDataURL(input.files[0]);	
 }
 
@@ -35,14 +24,11 @@ function startWorker() {
 	var workerIndex = index++;
 	var workerName = 'Worker' + workerIndex;
 	var worker =  new Worker(dataURL);
-	// alert('worker' + worker);
 	workers.set(workerName, worker);
-	
-	// alert('Posting message');
+
 	worker.postMessage([workerName, 'Start']);
 	
 	worker.addEventListener("message", e => {
-		// alert('Waiting for worker response');
 		if (e.data) {
 			const listItem = document.createElement("li"); 
 			listItem.textContent = e.data[0] + '|' + e.data[1];				
@@ -59,7 +45,6 @@ function stopWorkers() {
   while(workers.size > 0) {
 		var workerIndex = --index;
 		var workerName = 'Worker' + workerIndex;
-		// alert('Stopping worker' + workerName);
 		var worker = workers.get(workerName);
 		worker.postMessage([workerName, 'Stop']);		
 		workers.delete(workerName);		
